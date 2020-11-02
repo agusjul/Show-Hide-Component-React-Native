286677b30c431256468bf4c9d567fe6b80773836
bsand8813 <bsand8813@gmail.com>
2020-11-02 17:21:25 +0800

    adding input and value text via button

--- Button/App.js
+++ Button/App.js
@@ -1,14 +1,23 @@
 import React, {Component} from 'react';
 import * as eva from '@eva-design/eva';
-import { StyleSheet, View, StatusBar } from 'react-native';
-import { Button, Layout, Text,  ApplicationProvider} from '@ui-kitten/components';
+import { StyleSheet, View, StatusBar, Keyboard, Animated, ScrollView } from 'react-native';
+import { Button,  Text,  ApplicationProvider, Card, Input, useTheme} from '@ui-kitten/components';
 import { Icon } from 'react-native-eva-icons';
 
 export default class App extends Component {
-  state = {
-    tampil : false,
-    buttontext : "Tampilkan"
+  constructor(){
+    super();
+    this.state = {
+      tampil: false,
+      buttontext: "Tampilkan",
+      forms: '',
+      submittext: [],
+      index: 0,
+      disable : false
+    }
+    // this.animasiValue = new Animated.Value(0)
   }
+  
 
   presbutton = () =>{
     console.log("tombol ditekan");
@@ -30,12 +39,21 @@ export default class App extends Component {
     if(this.state.tampil){
       return(
         <View>
-          <Text>
+          <Text category="h2">
             Hello World
           </Text>
         </View>
       )
     }
+  }
+
+  sendIcon = () => {
+    return(
+      <React.Fragment>
+        <Icon name='paper-plane-outline' fill="white" width={22} height={22} />
+      </React.Fragment>
+    )
+    
   }
 
   ButtonIcon = () => {
@@ -54,20 +72,93 @@ export default class App extends Component {
     }
   }
 
+  formsChange = (event) => {
+    const { text } = event;
+    let processedData = text;
+    console.log(processedData);
+    this.setState({
+      forms : processedData
+    })
+    console.log("ini state = " + this.state.forms);
+  }
+
+  sendForms = (e) => {
+    Keyboard.dismiss();
+
+    let text = e;
+    this.setState({
+      submittext : text
+    })
+  }
+
+  message = (a) => {
+    let text = a;
+    return (
+      <React.Fragment>
+        <Card style={{ backgroundColor: 'white' }}>
+          <Text category="h6" style={{fontWeight : "bold"}}>{text}</Text>
+        </Card>
+      </React.Fragment>
+    )
+  }
+
   render(){
     return(
       <ApplicationProvider {...eva} theme={eva.light}>
-        <View style ={{padding : 10}}>
-          <StatusBar barStyle="dark-content" backgroundColor="#f7f9fc"
-          />
-          <Button status='primary' accessoryLeft={this.ButtonIcon} onPress={() => this.presbutton()}>
-            {this.state.buttontext}
-          </Button>
+      <View>
+        <View style={{padding : 10}}>
+          <Card style={{height : 170}}>
+              <View style={{ padding: 10 }}>
+                <StatusBar barStyle="dark-content" backgroundColor="#f7f9fc"
+                />
+                <Button status='primary' accessoryLeft={this.ButtonIcon} onPress={() => this.presbutton()}>
+                  {this.state.buttontext}
+                </Button>
+              </View>
+              <View style={{ padding: 10 }}>
+
+                {this.showText()}
+              </View>
+          </Card>
         </View>
         <View style={{ padding: 10 }}>
-        
-          {this.showText()}
+          <Card>
+            <Text>Input :</Text>
+            <View style={{display : "flex", flexDirection : "row"}}>
+              <View style={{width : "80%"}}>
+                  <Input
+                    size='large'
+                    placeholder=''
+                    onChangeText={text => this.formsChange({ text })}
+                    value = {this.state.forms}
+                    style={{
+                      borderTopRightRadius: 0,
+                      borderBottomRightRadius: 0
+                    }}
+
+                  />
+              </View>
+                <View style={{ width: "20%", padding: 0 }}>
+                  <Button 
+                    onPress={()=> this.sendForms(this.state.forms)}
+                    accessoryLeft={this.sendIcon}
+                    style={{
+                      borderTopLeftRadius : 0,
+                      borderBottomLeftRadius : 0
+                      }}>
+
+                  </Button>
+               </View>
+                
+            </View>
+            <View style={{paddingTop : 20}}>
+              <Text>Value :</Text>
+              {this.message(this.state.submittext)}
+            </View>
+          </Card>
         </View>
+      </View>
+        
       </ApplicationProvider>
       
     )
